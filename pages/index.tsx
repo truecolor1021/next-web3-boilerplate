3f869275981754e6636fb24bcf78bd452eac8824
TerryHart <true.color1021@gmail.com>
2024-10-04 06:21:22 +0900

    final

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,22 @@ function Home() {
         </nav>
       </header>
 
-   
+      <main>
+        <h1>
+          Welcome to{" "}
+          <a href="https://github.com/mirshko/next-web3-boilerplate">
+            next-web3-boilerplate
+          </a>
+        </h1>
+
+        {isConnected && (
+          <section>
+            <ETHBalance />
+
+            <TokenBalance tokenAddress={DAI_TOKEN_ADDRESS} symbol="DAI" />
+          </section>
+        )}
+      </main>
 
       <style jsx>{`
         nav {
